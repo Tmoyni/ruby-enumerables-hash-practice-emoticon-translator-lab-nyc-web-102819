3ca36137669e7c55aa11e6022a88409be0a5ff9d
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-06 19:22:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,6 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   restult = load_library(file_path)['get_emoticon'][emoticon]
     result ? result : "Sorry, that emoticon was not found"
-
 end
 
 def get_english_meaning(file_path, emoticon)

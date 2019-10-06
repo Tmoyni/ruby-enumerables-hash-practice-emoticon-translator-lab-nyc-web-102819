ba30dab88bf374ee9ed90340aede51ab30c5a271
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-06 19:16:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,11 +15,11 @@ def load_library(file_path)
 end 
 
 def get_japanese_emoticon(file_path, emoticon)
-  restult = load_library(file_path)['get_emoticon']
+  restult = load_library(file_path)['get_emoticon'][emoticon]
   
 end
 
 def get_english_meaning(file_path, emoticon)
-  load_library(file_path)
+   restult = load_library(file_path)['get_meaning'][emoticon]
   
 end
